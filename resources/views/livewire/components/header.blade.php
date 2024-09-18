609e0e534f351b6e746e30828323835a9c005f6a
10994015 <a0938599191@gmail.com>
2024-09-18 21:27:56 +0800

    更改Header

--- resources/views/livewire/components/header.blade.php
+++ resources/views/livewire/components/header.blade.php
@@ -1,23 +1,16 @@
-<div class="header">
-    <div class="header__logo">
-      <strong>Boying</strong>
-    </div>
-    <nav class="navbar">
-      <ul class="navbar__menu">
-        <li class="navbar__item">
-          <a href="#" class="navbar__link"><i data-feather="home"></i><span>首頁</span> </a>
-        @if(!Auth::check())
-        <li class="navbar__item">
-          <a href="#" class="navbar__link"><i data-feather="settings"></i><span>登入</span></a>
-        </li>
-        @else
-        <li class="navbar__item">
+
+  <header class="header-outer">
+    <div class="header-inner responsive-wrapper">
+      <div class="header-logo">
+        <a href="{{route('users')}}">HOME</a>
+      </div>
+      <nav class="header-navigation">
+        @if(Auth::check())
           <form action="{{route('logout')}}" method="post">
             @csrf
             <button type="submit" class="navbar__link"><i data-feather="settings"></i><span>登出</span></button>
           </form>
-        </li>
         @endif
-      </ul>
-    </nav>
-  </div>
+      </nav>
+    </div>
+  </header>
