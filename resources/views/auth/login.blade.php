a5baebaea55eae57d1615f3dcce80e497eb7233e
10994015 <a0938599191@gmail.com>
2024-09-21 22:33:29 +0800

    燈入頁面 英文便中文

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,12 +16,12 @@
             @csrf
 
             <div>
-                <x-label for="username" value="{{ __('username') }}" />
+                <x-label for="username" value="{{ __('帳號') }}" />
                 <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('密碼') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
@@ -33,11 +33,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                
 
                 <x-button class="ms-4">
                     {{ __('Log in') }}
